3d11ee6327ea66f9f4fb771e269477d5b35dc9b4
Muzaffer Onur DAĞHAN <modsoftcom@gmail.com>
2017-07-11 11:03:02 +0300

    - Library and Console App Layers has been created

---
+++ Turgo/Turgo.Con/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Turgo.Lib;
+
+namespace Turgo.Con
+{
+    class Program
+    {
+        static Connection connection;
+        static void Main(string[] args)
+        {
+            connection = new Connection();
+            showMenu();
+        }
+
+        private static void showMenu()
+        {
+                Console.WriteLine(":: BLUETOOTH DEVICE CONNECTION ::");
+            int selectedMenu = 0;
+
+            do
+            {
+                Console.WriteLine("********* MENU OPTIONS **********");
+                Console.WriteLine("1. Discover devices around");
+                Console.WriteLine("2. Show adapter info");
+                Console.WriteLine("3. Exit");
+                Console.Write("> Please select an option : ");
+                int.TryParse(Console.ReadLine(), out selectedMenu);
+                if (selectedMenu == 0)
+                    Console.WriteLine("Please enter a number!");
+                else if(selectedMenu < 1 || selectedMenu > 3)
+                    Console.WriteLine("Please enter a correct number!");
+
+            } while (selectedMenu < 1 || selectedMenu > 3);
+            switch (selectedMenu)
+            {
+                case 1:
+                    break;
+                case 2:
+                    Console.WriteLine("**** DEVICE ADAPTER INFO ****");
+                    break;
+                case 3:
+                    Environment.Exit(0);
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+}
---
+++ Turgo/Turgo.Lib/Connection.cs
@@ -0,0 +1,58 @@
+﻿using InTheHand.Net;
+using InTheHand.Net.Bluetooth;
+using InTheHand.Net.Sockets;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Turgo.Lib
+{
+    public class Connection
+    {
+        public BluetoothClient Client { get; set; }
+        public BluetoothDeviceInfo[] Peers { get; set; }
+        public BluetoothDeviceInfo Device { get; set; }
+        public BluetoothAddress Address { get; set; }
+
+        public Connection()
+        {
+            Client = new BluetoothClient();
+        }
+
+        /// <summary>
+        /// Discovers bluetooth devices around and set Peers property
+        /// </summary>
+        public void Discover()
+        {
+            Peers = Client.DiscoverDevices();
+            Console.WriteLine("Searching devices...");
+            int i = 0;
+            foreach (BluetoothDeviceInfo device in Peers)
+            {
+                Console.WriteLine(++i + " - " + device.DeviceName + " (" + device.DeviceAddress + ") - RSSI : " + device.Rssi);
+            }
+            Console.WriteLine("****** End of list ******");
+        }
+
+        /// <summary>
+        /// Uses for getting selected device
+        /// </summary>
+        /// <param name="index">Index number of selected device</param>
+        /// <returns>Selected bluetooth device</returns>
+        public BluetoothDeviceInfo getDevice(int index)
+        {
+            return Peers[index - 1];
+        }
+
+        /// <summary>
+        /// Uses for getting selected device address info
+        /// </summary>
+        /// <returns>Address information of selected device</returns>
+        public BluetoothAddress getDeviceAddress()
+        {
+            return Device.DeviceAddress;
+        }
+    }
+}
